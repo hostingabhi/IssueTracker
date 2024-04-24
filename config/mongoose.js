ad899cf0b7f73e1df6ba0a6c3b2dec3e89a8be98
hostingabhi <abhishekprajapat423@gmail.com>
2024-04-25 00:13:18 +0530

    COMMIT LAST

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://abhishekprajapat423:R1pYqxWgTpgN41si@cluster0.0feuhwc.mongodb.net/', { useNewUrlParser: true, useUnifiedTopology: true });
+const url = 'mongodb+srv://abhishekprajapat423:R1pYqxWgTpgN41si@cluster0.0feuhwc.mongodb.net/'
 
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'Error in connecting to MongoDB'));
-
-db.once('open', () => {
-  console.log('--> Connected to Database :: MongoDB <--');
-});
-
-module.exports = db;
+async function mongo() {
+        await mongoose.connect(url);
+  }
+mongo().then(()=>{
+    console.log('Connected to mongoDB')
+}).catch((error)=>{
+    console.log('Unable to connect to mongoDB', error)
+})
