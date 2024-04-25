5165dd04520b3920300221310f1a41bfe5aed04f
hostingabhi <abhishekprajapat423@gmail.com>
2024-04-25 18:57:19 +0530

    25-04-2024:7:00

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://abhishekprajapat423:R1pYqxWgTpgN41si@cluster0.0feuhwc.mongodb.net/', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb+srv://abhishekprajapat423:5PzLl3qqAYKFiDjQ@issutracker.of0ogs7.mongodb.net/issuetracker', { useNewUrlParser: true, useUnifiedTopology: true });
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'Error in connecting to MongoDB'));
